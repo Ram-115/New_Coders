45fd2e05cf70d2f0624105a4d334e104c9b62b46
Venkata Siva Rama Krishna Ya <ramakrishna>
2024-09-30 16:20:13 +0530

    Layout

--- projectz/src/Containers/Layout/NavBar/index.js
+++ projectz/src/Containers/Layout/NavBar/index.js
@@ -1,14 +1,20 @@
-import { AppBar, Avatar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Avatar, Grid, Stack, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const NavBar = () => {
-  return (
-    <AppBar position="fixed" sx={{ zIndex: 1201 }}>
-    <Toolbar>
-     <Avatar src='https://c4.wallpaperflare.com/wallpaper/146/782/552/nature-is-pleased-with-simplicity-and-nature-is-no-dummy-wallpaper-preview.jpg'/>
-    </Toolbar>
-  </AppBar>
-  )
+    return (
+        <AppBar position="fixed" sx={{ zIndex: 1201 }}>
+            <Toolbar>
+                <Grid container display={'flex'} justifyContent={'end'}>
+                    <Stack direction={'row'} spacing={2} alignItems={'center'}>
+                        <NotificationsIcon  sx={{color:'#fff'}}/>
+                        <Avatar src='https://c4.wallpaperflare.com/wallpaper/146/782/552/nature-is-pleased-with-simplicity-and-nature-is-no-dummy-wallpaper-preview.jpg' />
+                    </Stack>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default NavBar
