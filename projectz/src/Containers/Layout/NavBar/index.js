ea5d3e36fa7ca14592813ad259e6b2c19c93f176
Venkata Siva Rama Krishna Ya <ramakrishna>
2024-09-30 16:21:42 +0530

    Layout

--- projectz/src/Containers/Layout/NavBar/index.js
+++ projectz/src/Containers/Layout/NavBar/index.js
@@ -1,12 +1,16 @@
 import { AppBar, Avatar, Grid, Stack, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
     return (
         <AppBar position="fixed" sx={{ zIndex: 1201 }}>
             <Toolbar>
-                <Grid container display={'flex'} justifyContent={'end'}>
+                <Grid container display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
+                    <Stack>
+                      <MenuIcon  sx={{color:'#fff'}}/>  
+                    </Stack>
                     <Stack direction={'row'} spacing={2} alignItems={'center'}>
                         <NotificationsIcon  sx={{color:'#fff'}}/>
                         <Avatar src='https://c4.wallpaperflare.com/wallpaper/146/782/552/nature-is-pleased-with-simplicity-and-nature-is-no-dummy-wallpaper-preview.jpg' />
